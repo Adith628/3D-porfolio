c03da583c20cbb51664506ffa807bb89aa4aa893
Adithyan T <adithsreedhar628@gmail.com>
2023-05-26 17:08:34 +0530

    Hero component completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,105 @@ import { styled } from 'styled-components';
 
 const Section = styled.div`
   height: 100vh;
-
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 80vw;
+  display: flex;
+  justify-content: space-between;
+`
+const Left = styled.div`
+  flex : 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+`
+const Title = styled.h1`
+  font-size: 3.5rem;
+`
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+`
+const Line = styled.img`
+  height: 0.1rem;
+`
+const Subtitle = styled.h2`
+  color : #da4ea2;
+`
+const Desc = styled.p`
+  font-size: 1rem;
+  color: lightgray;
+
+`
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 400;
+  border: none;
+  width: 8rem;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
 `
 
+const Right = styled.div`
+  flex : 3;
+  position: relative;
+`
+const Img = styled.img`
+  width: min(90vh,45vw);
+  aspect-ratio: 1/1;
+  object-fit:contain;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left:0;
+  bottom: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @keyframes animate{
+    to{
+      transform: translateY(-10%);
+    }
+  }
+
+`
 
 const Hero = () => {
   return (
     <Section>
       <Navbar />
+      <Container>
+        <Left>
+          <Title>Think. Make. Solve</Title>
+          <WhatWeDo>
+            <Line src='./img/line.png' />
+            <Subtitle>
+              What we Do
+            </Subtitle>
+          </WhatWeDo>
+          <Desc>
+            We enjoy creating delightful, human-created digital experiences.
+          </Desc>
+          <Button>
+            Learn More
+          </Button>
+        </Left>
+        <Right>
+          {/* 3d Model */}
+          <Img src='./img/moon.png' />
+        </Right>
+      </Container>
     </Section>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,48 @@ const Section = styled.div`
     justify-content: center;
 `
 const Container = styled.div`
+  width: 80vw; 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+`
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  gap : 2rem;
+
+`
+const Logo = styled.img`
+height: 3rem;
+
+`
+const List = styled.ul`
+  display: flex;
+  gap: 2rem;
+  list-style: none;
+
+`
+const ListItem = styled.li`
+  cursor: pointer;
+`
+const Icon = styled.img`
+  height: 1.5rem;
+  cursor: pointer;
+`
+const Button = styled.button`
+  width: 5rem;
+  padding: 0.5rem;
+  background-color: #da4ea2;;
+  border: none;
+  color: white;
+  cursor: pointer;
+  border-radius: 0.25rem;
+`
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap : 2rem;
 
 `
 
@@ -13,7 +55,19 @@ const Navbar = () => {
   return (
     <Section>
         <Container>
-            <h1>Navbar</h1>
+            <Links>
+              <Logo src='./img/logo.png' />
+              <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Studio</ListItem>
+                <ListItem>Works</ListItem>
+                <ListItem>Contact</ListItem>
+              </List>
+            </Links>
+            <Icons>
+              <Icon src='./img/search.png' />
+              <Button>Hire Now</Button>
+            </Icons>
         </Container>
     </Section>
   )
