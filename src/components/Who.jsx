7eeda81768a723c6b9ec034d856a89622fd93b9c
Adithyan T <adithsreedhar628@gmail.com>
2023-05-28 15:43:12 +0530

    updated

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,15 +1,88 @@
-import React from 'react'
+import React from 'react';
 import { styled } from 'styled-components';
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 80vw;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Right = styled.div`
+  flex : 1;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+`
+
+const Title = styled.h1`
+  font-size: 3.5rem;
+  line-height: 1.2;
+`
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+`
+const Line = styled.img`
+  height: 0.1rem;
+`
+const Subtitle = styled.h2`
+  color : #da4ea2;
+`
+const Desc = styled.p`
+  font-size: 1rem;
+  color: lightgray;
+
+`
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 400;
+  border: none;
+  width: 8rem;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+`
+
+const Left = styled.div`
+  flex : 1;
+
 `
 
 const Who = () => {
   return (
     <Section>
-      Who
+      <Container>
+        <Left>
+          {/* 3d Model */}
+        </Left>
+        <Right>
+          <Title>Think outside the square space</Title>
+          <WhatWeDo>
+            <Line src='./img/line.png' />
+            <Subtitle>
+              Who we are
+            </Subtitle>
+          </WhatWeDo>
+          <Desc>
+             A create group of  designers and developers with a passion for the arts.
+          </Desc>
+          <Button>
+            See our works
+          </Button>
+        </Right>
+      </Container>
     </Section>
   )
 }
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,16 +1,72 @@
 import React from 'react'
 import { styled } from 'styled-components';
 
+
+const data = [
+  "Web Design",
+  "Development",
+  "Mobile App",
+  "Illustration",
+  "Branding",
+];
+
 const Section = styled.div`
   height: 100vh;
-
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+
+`
+const Container = styled.div`
+  width: 80vw;
+  display: flex;
+  justify-content: space-between;
+`
+const Left = styled.div`
+  flex : 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;  
+`
+const List = styled.ul`
+  list-style: none;
+  display: flex;  
+  flex-direction: column;
+  gap: 1rem;
+`
+const ListItem = styled.li`
+  font-size:3rem;
+  font-weight: 600;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 0.5px white;
+  
+  ::after{
+    content: "Test";
+
+  }
 `
 
+const Right = styled.div`
+  flex : 1;
+`
 
 const Works = () => {
   return (
-    <Section>Works</Section>
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item)=>(
+              <ListItem key = {item}>{item}</ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right>
+        </Right>
+      </Container>
+    </Section>
   )
 }
 
